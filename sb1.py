1c2fe1a04239903607cee72a5e0e531f5caff0b6
dodyfadhlurrahman <39489316+dodyfadhlurrahman@users.noreply.github.com>
2018-05-21 20:52:13 +0700

    Add files via upload

---
+++ sb1.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+from linepy import *
+import json, time, random
+
+client = LineClient()
+#client = LineClient(authToken='AUTH TOKEN')
+client.log("Auth Token : " + str(client.authToken))
+
+channel = LineChannel(client)
+client.log("Channel Access Token : " + str(channel.channelAccessToken))
+
+poll = LinePoll(client)
+
+cctv={
+    "cyduk":{},
+    "point":{},
+    "sidermem":{}
+}
+
+while True:
+    try:
+        ops=poll.singleTrace(count=50)
+        for op in ops:
+            if op.type == 26:
+                msg = op.message
+                if msg.text != None:
+                    if msg.toType == 2:
+                        may = client.getProfile().mid
+                        if may in str(msg.contentMetadata) and 'MENTION' in str(msg.contentMetadata):
+                            pilih = ['Ada apa sayang tumben ngetag, kangen ya ?','Cie Ngetag Kangen ya ?','Yang tag mudah mudahan jodohnya','Rindu itu berat kamu gk akan kuat']
+                            rslt = random.choice(pilih)
+                            client.sendText(msg.to, str(rslt))
+                        else:
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
+            elif op.type == 25:
+                msg = op.message
+                text = msg.text
+                msg_id = msg.id
+                receiver = msg.to
+                sender = msg._from
+                try:
+                    if msg.contentType == 0:
+                        if msg.toType == 2:
+                            client.sendChatChecked(receiver, msg_id)
+                            contact = client.getContact(sender)
+                            if text.lower() == 'dody':
+                                client.sendMessage(receiver, 'Nama Dody Fadhlurrahman, Umur 18 Tahun, Jabatan Owner Xcode Gen 2017', contentMetadata={'mid': sender}, contentType=13)
+                                
+                            elif                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                text.lower() == 'speed':
+                                start = time.time()
+                                client.sendText(receiver, "ResultSpeed")
+                                elapsed_time = time.time() - start
+                                client.sendText(receiver, "%sdetik" % (elapsed_time))
+                            elif 'spic' in text.lower():
+                                try:
+                                    key = eval(msg.contentMetadata["MENTION"])
+                                    u = key["MENTIONEES"][0]["M"]
+                                    a = client.getContact(u).pictureStatus
+                                    client.sendImageWithURL(receiver, 'http://dl.profile.line.naver.jp/'+a)
+                                except Exception as e:
+                                    client.sendText(receiver, str(e))
+                            elif 'scover' in text.lower():
+                                try:
+                                    key = eval(msg.contentMetadata["MENTION"])
+                                    u = key["MENTIONEES"][0]["M"]
+                                    a = channel.getProfileCoverURL(mid=u)
+                                    client.sendImageWithURL(receiver, a)
+                                except Exception as e:
+                                    client.sendText(receiver, str(e))
+                            elif text.lower() == 'tagall':
+                                group = client.getGroup(msg.to)
+                                nama = [contact.mid for contact in group.members]
+                                nm1, nm2, nm3, nm4, nm5, jml = [], [], [], [], [], len(nama)
+                                if jml <= 100:
+                                    client.mention(msg.to, nama)
+                                if jml > 100 and jml < 200:
+                                    for i in range(0, 100):
+                                        nm1 += [nama[i]]
+                                    client.mention(msg.to, nm1)
+                                    for j in range(101, len(nama)):
+                                        nm2 += [nama[j]]
+                                    client.mention(msg.to, nm2)
+                                if jml > 200 and jml < 300:
+                                    for i in range(0, 100):
+                                        nm1 += [nama[i]]
+                                    client.mention(msg.to, nm1)
+                                    for j in range(101, 200):
+                                        nm2 += [nama[j]]
+                                    client.mention(msg.to, nm2)
+                                    for k in range(201, len(nama)):
+                                        nm3 += [nama[k]]
+                                    client.mention(msg.to, nm3)
+                                if jml > 300 and jml < 400:
+                                    for i in range(0, 100):
+                                        nm1 += [nama[i]]
+                                    client.mention(msg.to, nm1)
+                                    for j in range(101, 200):
+                                        nm2 += [nama[j]]
+                                    client.mention(msg.to, nm2)
+                                    for k in range(201, len(nama)):
+                                        nm3 += [nama[k]]
+                                    client.mention(msg.to, nm3)
+                                    for l in range(301, len(nama)):
+                                        nm4 += [nama[l]]
+                                    client.mention(msg.to, nm4)
+                                if jml > 400 and jml < 501:
+                                    for i in range(0, 100):
+                                        nm1 += [nama[i]]
+                                    client.mention(msg.to, nm1)
+                                    for j in range(101, 200):
+                                        nm2 += [nama[j]]
+                                    client.mention(msg.to, nm2)
+                                    for k in range(201, len(nama)):
+                                        nm3 += [nama[k]]
+                                    client.mention(msg.to, nm3)
+                                    for l in range(301, len(nama)):
+                                        nm4 += [nama[l]]
+                                    client.mention(msg.to, nm4)
+                                    for m in range(401, len(nama)):
+                                        nm5 += [nama[m]]
+                                    client.mention(msg.to, nm5)             
+                                client.sendText(receiver, "//Created By Dody Fadhlurrahman// Members Grup :"+str(jml))
+                            elif text.lower() == 'sideron':
+                                try:
+                                    del cctv['point'][msg.to]
+                                    del cctv['sidermem'][msg.to]
+                                    del cctv['cyduk'][msg.to]
+                                except:
+                                    pass
+                                cctv['point'][msg.to] = msg.id
+                                cctv['sidermem'][msg.to] = ""
+                                cctv['cyduk'][msg.to]=True
+                            elif text.lower() == 'sideroff':
+                                if msg.to in cctv['point']:
+                                    cctv['cyduk'][msg.to]=False
+                                    client.sendText(msg.to, cctv['sidermem'][msg.to])
+                                else:
+                                    client.sendText(msg.to, "Heh Belum Juga Diset, Udh Main Offin Aja")
+                                    
+                except Exception as e:
+                    client.log("[SEND_MESSAGE] ERROR : " + str(e))
+
+            elif op.type == OpType.NOTIFIED_READ_MESSAGE:
+                try:
+                    if cctv['cyduk'][op.param1]==True:
+                        if op.param1 in cctv['point']:
+                            Name = client.getContact(op.param2).displayName
+                            if Name in cctv['sidermem'][op.param1]:
+                                pass
+                            else:
+                                cctv['sidermem'][op.param1] += "\n~ " + Name
+                                pref=['Ikut Chat Dong Kak, Gk Enak Tau Cuma Di Read...']
+                                client.sendText(op.param1, str(random.choice(pref))+' '+Name)
+                        else:
+                            pass
+                    else:
+                        pass
+                except:
+                    pass
+
+            else:
+                pass
+
+            # Don't remove this line, if you wan't get error soon!
+            poll.setRevision(op.revision)
+            
+    except Exception as e:
+        client.log("[SINGLE_TRACE] ERROR : " + str(e))
